936dd10e0221f217981697e9e2df78fd68d5c995
PandAJ2021 <pandaj200021@gmail.com>
2023-04-10 05:06:46 +0330

    file handler class was created

--- finance_manager/core/file_handler.py
+++ finance_manager/core/file_handler.py
@@ -1,1 +1,20 @@
-# I gonna use shelve
+import pickle
+
+class Pickle_db:
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def add_data(self , array):
+        with open(self.file_name, 'ab') as myfile:
+            pickle.dump(array, myfile)
+
+    def load_data(self):
+        objects = []
+        with (open(self.file_name, "rb")) as openfile:
+            while True:
+                try:
+                    objects.append(pickle.load(openfile))
+                except EOFError:
+                    break
+        return objects
+
